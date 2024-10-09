896102a4ee14e047bad64ab22209ea793eeb4de7
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-09 11:24:31 +0300

    Numbers operation

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let a = 3; // number
-let b = 1.2; // number
-let c = 'hello world'; // string
-let d = "hello world"; // string
-let e = true; // boolean
-let f = false; // boolean
-let g = null; // object ?
-let h = undefined;// undefined
-let j = 1/0; // infinity type number
-let k = -1/0;// -infinity type number
-let l = Math.sqrt(-1);// NaN (not a number) type number
-let m = [1, 1.2, 'asdasd', true]; // array
-let n = {name: 'kaspar', age: 31}; // object
-console.log(typeof n);
+let num = 3;
+num = num + 2;
+num += 2;
+num -= 3;
+num *= 4;
+num /= 2;
+num = 7 % 2;
+num = 6 % 4;
+num %= 2;
+num++;
+num--;
+num = Math.PI;
+num = Math.abs(-2);
+num = 2**5;
+console.log(num);
