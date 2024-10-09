21b6f9498a4dbb2b5af39296a7a3713661d7c87b
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-09 10:57:47 +0300

    Types

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-
-let a = 3;
-
-//var b = 1.2; dont use var use let
-
-console.log(a);
+let a = 3; // number
+let b = 1.2; // number
+let c = 'hello world'; // string
+let d = "hello world"; // string
+let e = true; // boolean
+let f = false; // boolean
+let g = null; // object ?
+let h = undefined;// undefined
+let j = 1/0; // infinity type number
+let k = -1/0;// -infinity type number
+let l = Math.sqrt(-1);// NaN (not a number) type number
+let m = [1, 1.2, 'asdasd', true]; // array
+let n = {name: 'kaspar', age: 31}; // object
+console.log(typeof n);
