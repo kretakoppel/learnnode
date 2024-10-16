dfd18b4736ead77adcc457890732d4db7947a3ef
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-16 11:36:08 +0300

    Add for loop example

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-const date = new Date('1993-03-21T12:00:00');
-console.log(date);
-console.log(date.getDay());
+for(let i=0;i<10;i++){
+    console.log(i);
+}
 
-switch(date.getDay()){
-    case 0:
-        console.log('Pühapäev');
-        break;
-    case 1:
-        console.log('Esmaspäev');
-        break;
-    case 2:
-        console.log('Teisipäev');
-        break;
-    case 3:
-        console.log('Kolmapäev');
-        
-    case 4:
-        console.log('Neljapäev');
-        break;
-    case 5:
-        console.log('Reede');
-        break;
-    case 6:
-        console.log('Laupäev');
-        break;
-    default:
-        console.log('WTF?');
-        break;        
+for(let i=9;i>=0;i--){
+    console.log(i);
+}
+for(let i=9;i<=0;i--){
+    console.log(i);
+}
+for(let i=9;i>=0;i++){
+    console.log(i);
 }
