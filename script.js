6fd76cf151316a12448db1fa2f66c1416ddf7580
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-16 10:46:26 +0300

    Boolean operations

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-let text = 'hello world';
-text = text + '!!!';
-text = '💩';
-text = text.length;
-console.log(text);
+let answer = !false // NOT
+answer = true && false; // AND
+answer = true && true;
+answer = false || true; // or
+answer = true && false || true && !false && !(true || false);
+answer = true ^ true; //XOR
+
+answer = 1 == 2;
+answer = '01' == 1; // checks values
+answer = '1' === 1; // checks types then values
+answer = '' == 0;
+answer = 'false' == false;
+answer = 1 != 2; // not equals
+answer = '2' !== 2;
+answer = 2 < 10;
+answer = 2 > 10;
+answer = 10 > 10;
+answer = 10 < 10;
+answer = 10 <= 10;
+answer = 10 >= 10;
+console.log(answer);
