08f3da85f746350167db3621a0c7d5ed34a48671
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-23 10:20:53 +0300

    for loops examples

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ for(let i=9;i>=0;i--){
 for(let i=9;i<=0;i--){
     console.log(i);
 }
-for(let i=9;i>=0;i++){
+// for(let i=9;i>=0;i++){ // infinite loop
+//     console.log(i);
+// }
+
+for(let i=0; i<=1000; i+=2){
     console.log(i);
 }
+for(let i=1; i<=1024; i*=2){
+    console.log(i);
+}
+
