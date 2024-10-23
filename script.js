118285b4230f2c0fe6eec95bd87a6e15c78ceaf0
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-23 11:14:20 +0300

    functions

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
-let array = ['banana', 'apple', 'cherry', 'pear'];
-
-for(value of array){
-    value = 'nothing';
-    console.log(value);
+function hello(){
+    console.log('Hello function');
 }
 
-console.log(array);
+hello();
+
+
+function greet(name='Nameless', age){
+    console.log('Hello ' + name + '!You are ' + age + ' years old');
+}
+
+greet('Kaspar', 30);
+greet('Martin', 21);
+greet();
+
+let sum = function (a, b){
+    return a+b;
+}
+
+let answer = sum(2,4);
+console.log(answer);
+
+let sub = (a,b) => {
+    return a-b;
+}
+
+answer = sub(2,4);
+console.log(answer);
+
+let div = (a,b) => a/b;
+
+answer = div(2,4);
+console.log(answer);
+
+let root = a => a*a;
+
+answer = root(6);
+console.log(answer);
 
 let object = {
-    name: 'kaspar',
-    age: 31,
-    color: 'green',
+    hello(){
+        console.log('hello object');
+    },
+    goodbye: () => {
+        console.log('goodbye object');
+    }
 }
 
-for(key in object){
-    console.log(key, object[key]);
+object.hello();
+object.goodbye();
+
+function isNameLong(name){
+    return name.length > 5;
 }
+
+console.log(isNameLong('Kaspar'));
+console.log(isNameLong('Ott'));
