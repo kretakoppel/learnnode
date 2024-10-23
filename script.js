a6c59ce03a6755ac0cd8ae630cf2d924a5d066a2
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-23 11:27:16 +0300

    recursive function

--- script.js
+++ script.js
@@ -1,57 +1,9 @@
-function hello(){
-    console.log('Hello function');
-}
-
-hello();
-
-
-function greet(name='Nameless', age){
-    console.log('Hello ' + name + '!You are ' + age + ' years old');
-}
-
-greet('Kaspar', 30);
-greet('Martin', 21);
-greet();
-
-let sum = function (a, b){
-    return a+b;
-}
-
-let answer = sum(2,4);
-console.log(answer);
-
-let sub = (a,b) => {
-    return a-b;
-}
-
-answer = sub(2,4);
-console.log(answer);
-
-let div = (a,b) => a/b;
-
-answer = div(2,4);
-console.log(answer);
-
-let root = a => a*a;
-
-answer = root(6);
-console.log(answer);
-
-let object = {
-    hello(){
-        console.log('hello object');
-    },
-    goodbye: () => {
-        console.log('goodbye object');
+function recursive(num){
+    console.log(num);
+    if(num<10){
+        // num++;
+        recursive(++num);
     }
 }
 
-object.hello();
-object.goodbye();
-
-function isNameLong(name){
-    return name.length > 5;
-}
-
-console.log(isNameLong('Kaspar'));
-console.log(isNameLong('Ott'));
+recursive(0);
