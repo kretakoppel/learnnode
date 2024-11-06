92507188dd6614ae9d07975cc01448286f4ef86e
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-11-06 11:27:03 +0200

    list example

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let button = document.querySelector('button');
-button.addEventListener('click', function (event) {
-    console.log(event);
-    let heading = document.querySelector('h1');
-    heading.innerHTML = 'Hello VM23';
-});
-
 let input = document.querySelector('input');
-input.addEventListener('input', function(event){
-    let text = document.querySelector('h1#text');
-    text.innerHTML = input.value.split('').reverse().join('');
+let list = document.querySelector('ul');
+
+let add = () => {
+    if(input.value.trim()){
+        let listItem = document.createElement('li');
+        listItem.innerHTML = input.value.trim();
+        list.append(listItem);   
+    }
+    input.value = '';
+}
+
+button.addEventListener('click', add);
+input.addEventListener('keydown', event => {
+    if(event.code === 'Enter'){
+        add();
+    }
 });
 
