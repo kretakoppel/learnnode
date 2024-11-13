7673468b833322c4bc23cf796dba0505b545fd09
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-11-13 10:47:27 +0200

    Weird tree

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
-context.moveTo(0,0);
-context.lineTo(300, 300);
+
+context.moveTo(640,720);
+context.lineTo(640, 620);
+tree(640, 620, 100, 10);
 context.stroke();
 
-context.beginPath();
-context.arc(600,600,50, 0, 2*Math.PI);
-context.stroke();
-
-context.font = "50px Arial";
-context.fillText('WOW CANVAS!', 600, 100);
-context.strokeText('WOW CANVAS!', 600, 200);
+function tree(x, y, length, steps){
+    if(steps>0){
+        context.moveTo(x, y);
+        context.lineTo(x+length, y-length);
+        tree(x+length, y-length, length-10, steps-1);
+        context.moveTo(x, y);
+        context.lineTo(x-length, y-length);
+        tree(x-length, y-length, length-10, steps-1);
+    }
+}
