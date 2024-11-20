5a59eb87e82b8a1ad1ee6c9d26014ef09e8845ef
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-11-20 10:29:17 +0200

    Commonjs vs es Modules

--- script.js
+++ script.js
@@ -1,39 +1,8 @@
-let canvas = document.querySelector('canvas');
-let context = canvas.getContext('2d');
-
-let draw = false;
-let drawSize = 30;
-function drawing(){
-    let rect = event.target.getBoundingClientRect();
-    let x = event.clientX - rect.left;
-    let y = event.clientY - rect.top;
-    context.beginPath();
-    context.arc(x, y, drawSize, 0, 2*Math.PI);
-    context.fill();
-}
-
-let size =  document.querySelector('input#size');
-
-size.addEventListener('input', () => {
-    drawSize = size.value;
-})
-
-canvas.addEventListener('mousemove', event => {
-    if(draw){
-       drawing();
-    }
-});
-
-canvas.addEventListener('mousedown', () => {
-    draw = true;
-    drawing();
-});
-
-canvas.addEventListener('mouseup', () => {
-    draw = false;
-});
-
-let color = document.querySelector('input#color');
-color.addEventListener('input', () => {
-    context.fillStyle = color.value;
-})
+// const common = require('./common.js');
+// common.hello();
+// const es = require('./es.js').default;
+// es.hello();
+import common from './common.js';
+common.hello();
+import es from './es.js';
+es.hello();
