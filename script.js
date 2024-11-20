5f8df8268826a8659d5e4bd468f4cf8a0801e62a
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-11-20 11:30:16 +0200

    Simple express server

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-// const common = require('./common.js');
-// common.hello();
-// const es = require('./es.js').default;
-// es.hello();
-import common from './common.js';
-common.hello();
-import es from './es.js';
-es.hello();
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(import.meta.dirname + '/views/index.html');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(import.meta.dirname + '/views/about.html');
+});
+
+app.get('/hello', (req, res) => {
+    res.sendFile(import.meta.dirname + '/views/about.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
