3c571d48cb1fcbc790a98a6be8ebc82749e71448
Kreta Koppel <60708307129@tptlive.ee>
2024-12-18 11:35:33 +0200

    minu ilus algus

--- script.js
+++ script.js
@@ -1,46 +1,77 @@
 import express from 'express';
-import nunjucks from 'nunjucks';
 import fs from 'fs';
+import nunjucks from 'nunjucks';
+
 const app = express();
-const port = 3000;
+const port = 3000
 
 nunjucks.configure('views', {
-  autoescape: true,
-  express: app
+    autoescape: true,
+    express: app
 });
 
-
 app.get('/', (req, res) => {
-  res.render('index.njk');
+    res.render('index.njk');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.njk');
+    res.render('about.njk');
 });
 
 app.get('/form', (req, res) => {
-  res.render('form.njk');
+    res.render('form.njk');
 });
 
 app.get('/answer', (req, res) => {
     res.render('answer.njk', req.query);
-});
+}); 
 
 app.get('/square', (req, res) => {
-  res.render('square.njk');
+    res.render('square.njk');
 });
 
 app.get('/squareanswer', (req, res) => {
-  let a = req.query.a;
-  let answers = {
-    a: a,
-    S: Math.pow(a, 2),
-    P: 4 * a,
-    V: Math.pow(a, 3)
-  };
-  res.render('squareanswer.njk', answers);
+    let a = req.query.a;
+    let answer = {
+        S: Math.pow(a, 2),
+        a: a,
+        V: Math.pow(a, 3),
+        P: 4 * a,
+    };
+    res.render('squareanswer.njk', answer);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+app.get('/circle', (req, res) => {
+    res.render('circle.njk');
 });
+
+app.get('/circleanswer', (req, res) => {
+    let a = req.query.a;
+    let answer = {
+        S: Math.pow(a, 2),
+        a: a,
+        V: Math.pow(a, 3),
+        P: 4 * a,
+    };
+    res.render('circleanswer.njk', answer);
+});
+
+app.get('/pythagoras', (req, res) => {
+    res.render('pythagoras.njk');
+});
+
+app.get('/pythagorasanswer', (req, res) => {
+    let a = req.query.a;
+    let answer = {
+        S: Math.pow(a, 2),
+        a: a,
+        V: Math.pow(a, 3),
+        P: 4 * a,
+    };
+    res.render('pythagorasanswer.njk', answer);
+});
+
+  app.listen(port, () => {
+    console.log(`example app listening on port http://localhost:${port}`)
+  });
+  
