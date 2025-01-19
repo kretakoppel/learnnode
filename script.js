48dba5096daefa6ad373e3dda1962fa88cafcffd
kretakoppel <kreta.koppel@tptlive.ee>
2025-01-19 23:48:08 +0200

    ma hakkan nutma2

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 1ccc09a (ma hakkan nutma)
 import express from 'express';
 import fs from 'fs';
 import nunjucks from 'nunjucks';
@@ -29,11 +24,7 @@ app.get('/form', (req, res) => {
 
 app.get('/answer', (req, res) => {
     res.render('answer.njk', req.query);
-<<<<<<< HEAD
 }); 
-=======
-});
->>>>>>> 1ccc09a (ma hakkan nutma)
 
 app.get('/square', (req, res) => {
     res.render('square.njk');
@@ -47,11 +38,7 @@ app.get('/squareanswer', (req, res) => {
         V: Math.pow(a, 3),
         P: 4 * a,
     };
-<<<<<<< HEAD
     res.render('squareanswer.njk', answer);
-=======
-    res.render('squareanswer.njk', req.query);
->>>>>>> 1ccc09a (ma hakkan nutma)
 });
 
 app.get('/circle', (req, res) => {
@@ -59,21 +46,12 @@ app.get('/circle', (req, res) => {
 });
 
 app.get('/circleanswer', (req, res) => {
-<<<<<<< HEAD
     let a = req.query.a;
     let answer = {
         S: Math.pow(a, 2),
         a: a,
         V: Math.pow(a, 3),
         P: 4 * a,
-=======
-    let r = req.query.r;
-    let answer = {
-        S: Math.pow(r, 2) * Math.PI,
-        r: r,
-        V: Math.pow(r, 2) * Math.PI * 4/3,
-        C: 2 * Math.PI * r,
->>>>>>> 1ccc09a (ma hakkan nutma)
     };
     res.render('circleanswer.njk', answer);
 });
@@ -84,19 +62,11 @@ app.get('/pythagoras', (req, res) => {
 
 app.get('/pythagorasanswer', (req, res) => {
     let a = req.query.a;
-<<<<<<< HEAD
     let answer = {
         S: Math.pow(a, 2),
         a: a,
         V: Math.pow(a, 3),
         P: 4 * a,
-=======
-    let b = req.query.b;
-    let answer = {
-        b: b,
-        a: a,
-        C: Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)),
->>>>>>> 1ccc09a (ma hakkan nutma)
     };
     res.render('pythagorasanswer.njk', answer);
 });
